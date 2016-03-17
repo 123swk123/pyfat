b7366246e308ab095e3d819a59d01dc33303d673
Chris Lalancette <clalancette@gmail.com>
2016-03-16 20:43:57 -0400

    Add in more checks.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- tests/common.py
+++ tests/common.py
@@ -1,2 +1,17 @@
-def check_nofiles(iso, filesize):
+def check_nofiles(fat, filesize):
     assert(filesize == 1474560)
+    assert(fat.bytes_per_sector == 512)
+    assert(fat.sectors_per_cluster == 1)
+    assert(fat.reserved_sectors == 1)
+    assert(fat.num_fats == 2)
+    assert(fat.max_root_dir_entries == 224)
+    assert(fat.sector_count == 2880)
+    assert(fat.media == 0xf0)
+    assert(fat.sectors_per_fat == 9)
+    assert(fat.sectors_per_track == 18)
+    assert(fat.num_heads == 2)
+    assert(fat.hidden_sectors == 0)
+    assert(fat.total_sector_count_32 == 0)
+    assert(fat.drive_num == 0)
+    assert(fat.boot_sig == 41)
+    assert(fat.size_in_kb == 1440)
